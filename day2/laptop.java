a6f986e826487890845af3e878a1e01284f35028
Hariharanvhariharan007 <153617620+Hariharanvhariharan007@users.noreply.github.com>
2023-12-13 15:39:03 +0530

    Add files via upload
    day2 laptop , student

---
+++ day2/laptop.java
@@ -0,0 +1,26 @@
+package week1.day2;
+
+public class laptop {
+	public static void main(String[] args) {
+		String laptopName="dell";
+		System.out.println("laptop ");
+		double laptopPrice=99.99;
+		System.out.println("laptop = "+laptopPrice);
+		char firstLetter='s';
+		System.out.println('s');
+		boolean isDamaged=true;
+		System.out.println("true");
+		float frequency=34.34566f;
+		System.out.println("frequency");
+		long imeiNumber=765566356666656656l;
+		System.out.println("imeiNumber");
+		int laptopType=234455;
+		System.out.println("laptopType");
+		short laptopNumber=7777;
+		System.out.println("laptopNumber");
+		
+	
+				
+	}
+
+}
---
+++ day2/student.java
@@ -0,0 +1,35 @@
+package week1.day2;
+
+public class student {
+	public static void main(String[] args) {
+		String studentName="virat";
+		System.out.println("student name = "+studentName);
+		int rollNumber=567787;
+		System.out.println("roll Number = "+rollNumber);
+		float studentHeight=5.8f;
+		System.out.println("student height  = "+studentHeight);
+		char studentClass='E';
+		System.out.println("student class = "+studentClass);
+		boolean studentPresend=true;
+		System.out.println("student present = "+studentPresend);
+		long studentMobileNumber=98765554444l;
+		System.out.println("student Mobile Number = "+studentMobileNumber);
+		int numberOfStudent=34;
+		System.out.println("number of student = "+numberOfStudent);
+		byte studentWeight=70;
+		System.out.println("student weight = "+studentWeight);
+		double studentPercentange=75.1;
+		System.out.println("student percentange = "+studentPercentange);
+		
+	
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}

3a77e68ad7d4a91459dbb544f553a4f194092947
Hariharanvhariharan007 <153617620+Hariharanvhariharan007@users.noreply.github.com>
2024-01-04 23:27:01 +0530

    Assisment oops concept

--- week5/Students.java
+++ week5/Students.java
@@ -1,5 +1,27 @@
 package week5;
 
 public class Students {
+	public void getStudentlnfo(String id) {
+		System.out.println("ID:"+id);
+		
 
+	}
+	public void getStudentlnfo(int ID,String name) {
+		System.out.println("ID:"+ID+"name:"+name);
+
+	}
+	
+	
+	public void getSTudentlnfo(long phoneNumber,String email) {
+		System.out.println("phoneNumber:"+phoneNumber+"email:"+email);
+	}
+	public static void main(String[] args) {
+		Students leo=new Students();
+		leo.getStudentlnfo("20ghjd38");
+		leo.getStudentlnfo(12233, "thala");
+		leo.getSTudentlnfo(9887778777L," rx100hhdh@gmail.com");
+		
+		
+		
+	}
 }
